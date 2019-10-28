f5e588ec7a71a7bdf7ea249ddcf395f70d4cc196
[Orihata] <[overture76@gmail.com]>
2019-10-28 15:01:37 +0900

    changed unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ working_directory "#{app_path}/current"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 timeout 60
 
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
