9317d62bc2757acae580f1e0481053628145bd62
[Orihata] <[overture76@gmail.com]>
2019-10-28 15:21:16 +0900

    delete incorrect socket path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,6 @@ working_directory "#{app_path}/current"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
-listen "#{app_path}/tmp/sockets/unicorn.sock"
 timeout 60
 
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
