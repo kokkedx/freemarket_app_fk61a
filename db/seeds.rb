a8e64ae9150f0e5948bf99d7b8bca6b05affeb77
[Orihata] <[overture76@gmail.com]>
2019-11-04 19:47:21 +0900

    made structures of database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'jp_prefecture'
+
+# 都道府県リストを作る
+47.times do |number|
+  pref = JpPrefecture::Prefecture.find code: (number + 1)
+  Prefecture.create(name: "#{pref.name}")
+end
